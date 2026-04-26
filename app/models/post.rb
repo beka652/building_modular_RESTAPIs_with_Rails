a04f165685f8418bf652cc417d619f257accd859
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-04-26 13:43:16 +0300

    Add validations to the models.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+
+  validates :content, presence: true, length: { in: 5..100 }
+
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+
+  validates :email, presence: true, uniqueness: true
+  validates_format_of :email, with: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+  has_many :comments, dependent: :destroy
+  has_many :posts, dependent: :destroy
 end
