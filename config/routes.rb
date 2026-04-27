4de73c8331ea93bd54d9c56204923ebd76751943
Bereket Nebiyu <bereketnebiyu5@gmail.com>
2026-04-27 14:47:29 +0300

    create a method to delete users.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [ :create, :update, :show, :index ]
+      resources :users, only: [ :create, :update, :show, :index, :destroy ]
       resources :posts, only: [ :create, :update ]
       resources :comments, only: [ :create, :update ]
     end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -48,4 +48,9 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     get api_v1_users_url, as: :json
     assert_response :ok
   end
+
+  test "should destroy user" do
+    delete api_v1_user_url(@user), as: :json
+    assert_response :no_content
+  end
 end
